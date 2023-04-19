b3cef1532fc3e53988d2d603b2ade18e39a11618
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-19 09:02:32 +0300

    create nav

--- src/layouts/nav/nav.jsx
+++ src/layouts/nav/nav.jsx
@@ -1,7 +1,11 @@
+import Logo from "../../components/logo/";
+import styles from "./nav.module.scss";
+
 export default function Nav() {
   return (
-    <nav>
-      <h1>Hello World!</h1>
+    <nav className={styles.nav}>
+      <Logo className={styles.logo} />
+      <i className={`bx bx-menu ${styles.icon}`} />
     </nav>
   );
 }
