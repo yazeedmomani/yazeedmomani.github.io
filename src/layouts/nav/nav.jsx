dacd40758069ea8a4293fec53839f2fb0b6bf445
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-19 16:10:36 +0300

    add styles to nav

--- src/layouts/nav/nav.jsx
+++ src/layouts/nav/nav.jsx
@@ -2,10 +2,17 @@ import Logo from "../../components/logo/";
 import styles from "./nav.module.scss";
 
 export default function Nav() {
+  function handleMenu(e) {
+    //TODO Open menu
+  }
+
   return (
     <nav className={styles.nav}>
       <Logo className={styles.logo} />
-      <i className={`bx bx-menu ${styles.icon}`} />
+      <i
+        className={`bx bx-menu ${styles.icon}`}
+        onClick={handleMenu}
+      />
     </nav>
   );
 }
