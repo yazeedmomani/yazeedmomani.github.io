cf8ec80692b7be2869332835c3661cc3283f81a5
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-19 03:12:11 +0300

    replace app with root component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import "./styles/variables.scss";
 import "./styles/typography.scss";
 import "./styles/index.scss";
 
-import App from "./app";
+import Root from "./routes/root/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
