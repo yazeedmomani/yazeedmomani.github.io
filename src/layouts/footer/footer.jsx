724cf648a97841c21a18c254cda1fb426a190e8b
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-19 17:34:58 +0300

    add social media icons

--- src/layouts/footer/footer.jsx
+++ src/layouts/footer/footer.jsx
@@ -2,12 +2,39 @@ import styles from "./footer.module.scss";
 import Logo from "../../components/logo";
 
 export default function Footer() {
+  const twitterClass = `bx bxl-twitter ${styles.icon}`;
+  const linkedInClass = `bx bxl-linkedin ${styles.icon}`;
+  const githubClass = `bx bxl-github ${styles.icon}`;
+  const upworkClass = `bx bxl-upwork ${styles.icon}`;
+
   return (
     <footer className={styles.footer}>
       <Logo
         dark
         className={styles.logo}
       />
+      <div className={styles.iconContainer}>
+        <a
+          href="https://twitter.com/yaz_momani"
+          className={styles.link}>
+          <i class={twitterClass}></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/yazeedalmomani"
+          className={styles.link}>
+          <i class={linkedInClass}></i>
+        </a>
+        <a
+          href="https://github.com/yazeedmomani"
+          className={styles.link}>
+          <i class={githubClass}></i>
+        </a>
+        <a
+          href="https://www.upwork.com/freelancers/~01428ceffca847ca7a"
+          className={styles.link}>
+          <i class={upworkClass}></i>
+        </a>
+      </div>
     </footer>
   );
 }
