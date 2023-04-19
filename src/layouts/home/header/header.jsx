fe5e7c5d79a557811b2e52b6c022ce464f917157
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-19 20:11:26 +0300

    create header

--- src/layouts/home/header/header.jsx
+++ src/layouts/home/header/header.jsx
@@ -1,5 +1,13 @@
 import styles from "./header.module.scss";
 
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className={styles.header}>
+      <h1>
+        Hello, It's Me <span>Yazeed</span> And I am a{" "}
+        <span>Front-End Developer</span>
+      </h1>
+      <span>Designing Tomorrow's Web, One Pixel at a Time</span>
+    </header>
+  );
 }
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,5 +1,10 @@
 import Header from "../../layouts/home/header";
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <main className={styles.main}>
+      <Header />
+    </main>
+  );
 }
