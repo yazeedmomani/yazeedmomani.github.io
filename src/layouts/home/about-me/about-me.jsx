7503d7809c1ce3f695c64ba69e061fbc18192d7b
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-20 15:45:34 +0300

    add paragraph

--- src/layouts/home/about-me/about-me.jsx
+++ src/layouts/home/about-me/about-me.jsx
@@ -9,6 +9,12 @@ export default function AboutMe() {
         title="Who am I?"
         className={styles.sectionHeader}
       />
+      <p>
+        I am a skilled and dedicated React Front-End Web Developer with a
+        passion for creating engaging, user-friendly, and high-performance web
+        solutions. I empower businesses and individuals to elevate their digital
+        presence with expertly crafted React front-end web interfaces.
+      </p>
     </section>
   );
 }
