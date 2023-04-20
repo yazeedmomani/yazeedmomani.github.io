f300bcf96acbd085f5e7e6ec0e3162e7c468f137
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-20 22:33:20 +0300

    make breadcrumb dynamic

--- src/components/breadcrumb/breadcrumb.jsx
+++ src/components/breadcrumb/breadcrumb.jsx
@@ -1,14 +1,44 @@
 import styles from "./breadcrumb.module.scss";
 import { Link } from "react-router-dom";
 
-export default function Breadcrumb() {
+function renderItem(itemsLength, currentItem, currentIndex) {
+  if (itemsLength - 1 === currentIndex) return <span>{currentItem.text}</span>;
+
+  return (
+    <>
+      <Link to={currentItem.to}>{currentItem.text}</Link>
+      <i className="bx bx-chevron-right"></i>
+    </>
+  );
+}
+
+export default function Breadcrumb({ items }) {
+  const itemsLength = items.length;
+
   return (
     <div className={styles.breadcrumb}>
-      <Link>Home</Link>
-      <i className="bx bx-chevron-right"></i>
-      <Link>Projects</Link>
-      <i className="bx bx-chevron-right"></i>
-      <span>Josor Steels</span>
+      {items.map((current, index) => renderItem(itemsLength, current, index))}
     </div>
   );
 }
+
+/*
+NOTE Below is an example of how `items` should look like
+
+items = [
+    {
+        text: "Home",
+        to: "/",
+    },
+    {
+        text: "Projects",
+        to: "/projects",
+    },
+    {
+        text: "Josor Steels",
+    }
+]
+
+NOTE Last item in items always does not have a `to` property.
+
+*/
--- src/routes/about/about.jsx
+++ src/routes/about/about.jsx
@@ -1,9 +1,19 @@
 import Breadcrumb from "../../components/breadcrumb";
 
+const BREADCRUMB_ITEMS = [
+  {
+    text: "Home",
+    to: "/",
+  },
+  {
+    text: "About",
+  },
+];
+
 export default function About() {
   return (
     <main>
-      <Breadcrumb />
+      <Breadcrumb items={BREADCRUMB_ITEMS} />
     </main>
   );
 }
