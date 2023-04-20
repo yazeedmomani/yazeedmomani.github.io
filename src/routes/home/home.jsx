d13ec0bacb37b5d6429b34bb00c411ca13001cb4
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-20 16:20:50 +0300

    move cta to general folder

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -2,7 +2,7 @@ import Header from "../../layouts/home/header";
 import HowItWorks from "../../layouts/home/how-it-works";
 import Portfolio from "../../layouts/home/portfolio";
 import AboutMe from "../../layouts/home/about-me";
-import CTA from "../../layouts/home/cta";
+import CTA from "../../layouts/general/cta";
 
 export default function Home() {
   return (
