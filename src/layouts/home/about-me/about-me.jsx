6f34dba67e5027dc2ccc5f57f0fdb02b3c22701b
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-20 15:42:47 +0300

    add section header

--- src/layouts/home/about-me/about-me.jsx
+++ src/layouts/home/about-me/about-me.jsx
@@ -1,5 +1,14 @@
 import styles from "./about-me.module.scss";
+import SectionHeader from "../../../components/section-header";
 
 export default function AboutMe() {
-  return <h1>About Me</h1>;
+  return (
+    <section className={styles.section}>
+      <SectionHeader
+        name="About Me"
+        title="Who am I?"
+        className={styles.sectionHeader}
+      />
+    </section>
+  );
 }
