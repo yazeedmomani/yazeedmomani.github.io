5ae9df1011b870c5b68a97b9ff6cf9a7a85fa415
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-21 19:55:25 +0300

    create useWindowDimensions hook

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../../layouts/general/nav";
 import Footer from "../../layouts/general/footer";
-import useScrollToTop from "../../hooks/scroll-to-top";
+import useScrollToTop from "../../hooks/use-scroll-to-top";
 
 export default function Root() {
   useScrollToTop();
