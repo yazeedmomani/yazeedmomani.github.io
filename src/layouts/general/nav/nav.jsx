3587d031796c3023ae2d463e63af0e6df6f215fd
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-21 20:45:15 +0300

    make nav responsive

--- src/layouts/general/nav/nav.jsx
+++ src/layouts/general/nav/nav.jsx
@@ -3,9 +3,41 @@ import Logo from "../../../components/logo";
 import Menu from "../menu";
 import styles from "./nav.module.scss";
 import { Link } from "react-router-dom";
+import Button from "../../../components/button";
+import useWindowDimensions from "../../../hooks/use-window-dimensions";
+
+function renderLinks(viewportWidth, handleMenu) {
+  if (viewportWidth >= 900)
+    return (
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Button
+            primary
+            href="https://www.upwork.com/freelancers/~01428ceffca847ca7a"
+            target="_blank">
+            Hire me
+          </Button>
+        </li>
+      </ul>
+    );
+
+  return (
+    <i
+      className={`bx bx-menu ${styles.icon}`}
+      onClick={handleMenu}
+    />
+  );
+}
 
 export default function Nav() {
   const [showMenu, setShowMenu] = useState(false);
+  const { width } = useWindowDimensions();
 
   function handleMenu() {
     setShowMenu(true);
@@ -16,10 +48,7 @@ export default function Nav() {
       <Link to="/">
         <Logo className={styles.logo} />
       </Link>
-      <i
-        className={`bx bx-menu ${styles.icon}`}
-        onClick={handleMenu}
-      />
+      {renderLinks(width, handleMenu)}
       {showMenu && <Menu setShowMenu={setShowMenu} />}
     </nav>
   );
