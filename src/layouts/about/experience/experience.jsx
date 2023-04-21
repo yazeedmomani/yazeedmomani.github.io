1b57d13f37238327e7eac9219c61fa18a59c0dbc
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-21 13:31:18 +0300

    complete experience section

--- src/layouts/about/experience/experience.jsx
+++ src/layouts/about/experience/experience.jsx
@@ -19,6 +19,27 @@ export default function Experience() {
         ]}
         className={styles.multiInfoHeader}
       />
+      <p>
+        The company "Josor Steels" needed a new website to replace their old
+        one. They also needed someone to redraw their logo so that it is
+        scalable. I was tasked with designing the website, redrawing its logo,
+        and developing it.
+      </p>
+      <p>
+        I redrew the logo using Adobe Illustrator with a light background, a
+        dark background, and a transparent background and exported it in
+        different formats including SVG, PNG, and ICO. Then, I proceeded by
+        designing the website using Adobe XD. After that, I developed the
+        website using ReactJS, taking the mobile-first approach.
+      </p>
+      <p>
+        The website hosted on{" "}
+        <a href="https://yazeedmomani.github.io/josor-steels">
+          https://yazeedmomani.github.io/josor-steels
+        </a>{" "}
+        is the result of my work. All that is left is to add the contents and
+        deploy it.
+      </p>
     </section>
   );
 }
