c1e284ca42b83ee8668f43b6c03d992aa825f008
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-21 18:53:50 +0300

    add content

--- src/layouts/project/description/description.content.js
+++ src/layouts/project/description/description.content.js
@@ -2,6 +2,19 @@ const data = [
   {
     id: "wrk_web_josor-steels",
     name: "Josor Steels",
+    image: require("../../../images/josor-steels.png"),
+    alt: "Blueprint of a hunger watermarked by the logo of Josor Steels company",
+    statements: [
+      "Freelancer | Front-End Web Developer",
+      "Feb 2023 - Present",
+      "Amman, Jordan - Remote",
+    ],
+    paragraphs: [
+      'The company "Josor Steels" needed a new website to replace their old one. They also needed someone to redraw their logo so that it is scalable. I was tasked with designing the website, redrawing its logo, and developing it.',
+      "I redrew the logo using Adobe Illustrator with a light background, a dark background, and a transparent background and exported it in different formats including SVG, PNG, and ICO. Then, I proceeded by designing the website using Adobe XD. After that, I developed the website using ReactJS, taking the mobile-first approach.",
+      "The website hosted on the link below is the result of my work. All that is left is to add the contents and deploy it",
+    ],
+    link: "yazeedmomani.github.io/wrk_web_josor-steels",
   },
 ];
 
