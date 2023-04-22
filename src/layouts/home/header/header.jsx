82b4e2b3ec26145371ee6ba92921fe9a970c9b06
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-22 16:25:39 +0300

    animate header

--- src/layouts/home/header/header.jsx
+++ src/layouts/home/header/header.jsx
@@ -1,27 +1,60 @@
 import styles from "./header.module.scss";
 import Button from "../../../components/button";
+import Fade from "react-reveal/Fade";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <h1>
-        Hello, It's Me <span>Yazeed</span> And I am a{" "}
-        <span>Front-End Developer</span>
+        <Fade
+          duration={1000}
+          distance="80px"
+          delay={1400}
+          left>
+          <span>Hello, It's Me </span>
+        </Fade>
+        <Fade
+          duration={1000}
+          distance="80px"
+          delay={1400}
+          right>
+          <span className={`${styles.primary} ${styles.large}`}>Yazeed</span>
+        </Fade>{" "}
+        <Fade
+          duration={1000}
+          distance="80px"
+          delay={1400}
+          left>
+          <span>And I am a </span>{" "}
+          <span className={styles.primary}>Front-End Developer</span>
+        </Fade>
       </h1>
-      <span>Designing Tomorrow's Web, One Pixel at a Time</span>
-      <div>
-        <Button
-          primary
-          href="https://www.upwork.com/freelancers/~01428ceffca847ca7a"
-          target="_blank">
-          Hire me
-        </Button>
-        <Button
-          download
-          href={`${process.env.PUBLIC_URL}/yazeed-al-momani.pdf`}>
-          Resume
-        </Button>
-      </div>
+      <Fade
+        duration={1000}
+        distance="80px"
+        delay={1400}
+        right>
+        <span>Designing Tomorrow's Web, One Pixel at a Time</span>
+      </Fade>
+      <Fade
+        duration={1000}
+        distance="80px"
+        delay={1400}
+        bottom>
+        <div>
+          <Button
+            primary
+            href="https://www.upwork.com/freelancers/~01428ceffca847ca7a"
+            target="_blank">
+            Hire me
+          </Button>
+          <Button
+            download
+            href={`${process.env.PUBLIC_URL}/yazeed-al-momani.pdf`}>
+            Resume
+          </Button>
+        </div>
+      </Fade>
     </header>
   );
 }
